b8c6c7c76d017525d381d075a2e0dd197236e592
dokyun-kim4 <donny5557@gmail.com>
2026-02-01 20:42:27 -0500

    implement simulation loop

--- src/main.py
+++ src/main.py
@@ -2,24 +2,32 @@
 Main file for running the simulator.
 """
 
+import pickle
+import pandas as pd
+import csv
 from environment import Environment
 from robot import Robot
 from utils import Position, Pose, Landmark, Bounds
 
 if __name__ == "__main__":
     # set up the environment
-    # TODO: choose values for each input parameter, using the expected datatype
-    dimensions = None
-    dt = None
-    obstacles = []
-    landmarks = []
-    initial_robot_pose = None
+    dimensions = Bounds(0, 10, 0, 10)
+    dt = 0.1
+    obstacles = [Bounds(5, 7, 5, 7), Bounds(0, 4, 6, 8)]
+    landmarks = [
+                    Landmark(Position(2,2), 1), 
+                    Landmark(Position(5,5), 2),
+                    Landmark(Position(8,8), 3)
+                ]
+    lm_max_range = 3.0
+    initial_robot_pose = Pose(Position(0, 0), 0.6)
 
     env = Environment(
         dimensions,
         dt,
         obstacles,
         landmarks,
+        lm_max_range,
         initial_robot_pose,
     )
 
@@ -27,34 +35,42 @@ if __name__ == "__main__":
     robot = Robot(env)
 
     # set up timekeeping
-    # TODO: set the total_seconds variable to however long you want the simulator to run (not real-time!)
-    total_seconds = None
+    total_seconds = 30
     total_timesteps = total_seconds / env.DT
+    terminal = False
 
     # set up logging
-    ground_truth_history = []
-    sensor_data_history = []
+    ground_truth_history = pd.DataFrame()
+    sensor_data_history = pd.DataFrame()
 
     # set up input filepath and output filepaths
-    input_commands_filepath = ""
-    output_ground_truth_filepath = ""
-    output_sensor_data_filepath = ""
+    input_commands_filepath = "input/vel_cmd_example.csv"
+    output_ground_truth_filepath = "output/ground_truth.pkl"
+    output_sensor_data_filepath = "output/sensor_data.pkl"
 
     # open up the instructions, pop the first
     with open(input_commands_filepath, "r") as cmd:
-        # iterate through each timestep
+
+        # pop motor commands
+        csv_reader = csv.reader(cmd)
+        next(csv_reader, None)  # skip header row
+        next_cmd = next(csv_reader)
+        start_time = float(next_cmd[0])
+        next_lin_vel, next_ang_vel = float(next_cmd[1]), float(next_cmd[2])
+
         for step in range(int(total_timesteps) + 1):
-            # TODO: take a ground truth snapshot and add it to the history
+            ground_truth_history = pd.concat([ground_truth_history, env.take_state_snapshot()])
+            sensor_data_history = pd.concat([sensor_data_history, robot.take_sensor_measurements()])
+            
+            if not terminal and start_time >= step * env.DT:
+                try:
+                    next_cmd = next(csv_reader)
+                    start_time = float(next_cmd[0])
+                    next_lin_vel, next_ang_vel = float(next_cmd[1]), float(next_cmd[2])
+                except StopIteration:
+                    terminal = True
+            robot.robot_step_differential(next_lin_vel, next_ang_vel)
 
-            # TODO: take sensor measurements and add it to the history
-
-            # TODO: retrieve the next motor command from the input file
-
-            # TODO: execute the motor command
-
-    # at the end, write the histories into output files
-    with open(output_ground_truth_filepath, "w") as gt_data:
-        # TODO: write ground_truth_history to a file
-
-    with open(output_sensor_data_filepath, "w") as sensor_data:
-        # TODO: write sensor_data_history to a file
+    pickle.dump(ground_truth_history, open(output_ground_truth_filepath, "wb"))
+    pickle.dump(sensor_data_history, open(output_sensor_data_filepath, "wb"))
+    env.get_environment_info()
